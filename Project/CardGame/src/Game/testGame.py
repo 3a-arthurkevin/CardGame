5953b10ca1733cfb2f2b719c27242f676cb35f6b
3a-arthurkevin <torrente.arthur@gmail.com>
2014-06-29 12:45:57 +0200

    TEst module

---
+++ Project/CardGame/src/Game/testGame.py
@@ -0,0 +1,17 @@
+'''
+Created on 29 juin 2014
+
+@author: Arthur
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        self.toto = 1
