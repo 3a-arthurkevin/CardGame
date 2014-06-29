67e88182649dd251d4fb837a1eb64b904e4af390
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-06-29 18:40:42 +0200

    commit doc

---
+++ Project/CardGame/src/Card.py
@@ -0,0 +1,11 @@
+class Card:
+    """
+    Classe mere pour Serviteur et Item
+    """
+    
+    def __init__(self, name="No Name", description="No Description", cost=1):
+        self.name = name
+        self.description = description
+        self.cost = cost
+        
+    
