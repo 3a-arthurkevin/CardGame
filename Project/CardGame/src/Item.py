5cf7d3e3ea84771b2e8b5d444d43ac00b9bcb091
3a-arthurkevin <torrente.arthur@gmail.com>
2014-06-29 14:42:33 +0200

    Ajout enum dans ITem

--- Project/CardGame/src/Item.py
+++ Project/CardGame/src/Item.py
@@ -3,3 +3,21 @@ Created on 29 juin 2014
 
 @author: Kevin
 '''
+
+def enum(enumName, *listValueNames):
+    listValueNumbers = range(len(listValueNames))
+    dictAttrib = dict( zip(listValueNames, listValueNumbers) )
+    dictReverse = dict( zip(listValueNumbers, listValueNames) )
+    dictAttrib["dictReverse"] = dictReverse
+    return type(enumName, (), dictAttrib)
+
+# Remplacer par l'enum pour Item
+Direction = enum(
+    "Direction", #Nom de l'enum
+    "UP", "DOWN", "LEFT", "RIGHT" # Valeur de l'enum
+    )
+
+class Item:
+    
+    def __init__(self):
+        self.hp = 1
