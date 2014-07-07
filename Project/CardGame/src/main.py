4e313cc0298346cd81e0712e2c536a6b520fc321
Kevin WATHTHUHEWA <wathkevin@gmail.com>
2014-07-07 22:54:56 +0200

    commit detail

--- Project/CardGame/src/main.py
+++ Project/CardGame/src/main.py
@@ -13,24 +13,3 @@ if __name__ == '__main__':
     playerTwoName = "Player2"
     #game = Game({"playerName1" : playerOneName, "playerName2" : playerTwoName})
     #game.loop()
-    
-    
-    """
-    Loop ouverture du jeu
-        -->affichage des menus
-            -->faire un deck
-            -->loop création deck
-                -->save
-                -->load
-                -->retour
-            -->Faire un combat
-                -->1 joueur
-                -->2 joueur
-                -->Loop jeu
-                    -->joueur entre son nom
-                    -->joueur choisi sont deck
-                    -->lancement nouvelle interface
-                        -->combat
-                        -->Fin partie
-                        -->retour au menu (boucle debut)
-    """
