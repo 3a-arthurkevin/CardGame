444bd3b17ce220e86f0ed9c9770aa703b25285c7
3a-arthurkevin <torrente.arthur@gmail.com>
2014-07-08 12:11:10 +0200

    Fin docstring weapon

--- Project/CardGame/src/Weapon.py
+++ Project/CardGame/src/Weapon.py
@@ -10,6 +10,10 @@ Enumération des type d'arme disponible dans le jeu
 WeaponType = enum("WeaponType",
                   "SWORD", "AXE", "LANCE", "BOW", "EMAGIC", "LMAGIC", "NONE")
 
+
+"""
+Nom des arme associé à leurs type
+"""
 dicWeaponToString = {WeaponType.SWORD   : "Epée",
                      WeaponType.AXE     : "Hache",
                      WeaponType.LANCE   : "Lance",
@@ -20,13 +24,22 @@ dicWeaponToString = {WeaponType.SWORD   : "Epée",
 class Weapon(Card):
     
     def __init__(self, params):
+        """
+        Création d'une arme
+        """
         Card.__init__(self, params)
         self.stats = Stats(params.get("Stats"))
         self.durability = params.get("Durability")
         self.weaponType = params.get("WeaponType")
         
-    def useWeapon(self, servant):
+    def useWeapon(self):
+        """
+        Réduit la durabilité d'une arme
+        """
         self.durability -= 1
         
     def __str__(self):
+        """
+        fonction d'affichage d'une arme
+        """
         return Card.__str__(self) + self.stats.__str__()
